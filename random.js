0e127c66b2702e694e77cf742972c55b8726215d
jyjkuo <46705392+jyjkuo@users.noreply.github.com>
2022-02-02 10:07:27 -0800

    Create random.js

---
+++ random.js
@@ -0,0 +1,4 @@
+var foodItems = ["Bannana", "Apple", "Orange"];
+var theFood = foodItems[Math.floor(Math.random() * foodItems.length)];
+/* Will pick a random number from the length of the array and will go to the
+corosponding number in the array E.G: 0 = Bannana */
