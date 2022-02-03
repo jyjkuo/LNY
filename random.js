dca3c0217156a92fc8ae44109db5c76e79815cb9
jyjkuo <46705392+jyjkuo@users.noreply.github.com>
2022-02-03 00:27:50 -0800

    Delete random.js

--- random.js
+++
@@ -1,6 +0,0 @@
-var song = Array("song1", "song2", "song3", "song4", "song5", "song6");
-
-function randomSong() {
-  var randomSong = song[Math.floor(Math.random() * song.length)];
-  document.getElementById('randomSong').value = randomSong;
-}
