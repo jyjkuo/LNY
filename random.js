c4a891d66abdcd5739d61169032c6994d6466357
jyjkuo <46705392+jyjkuo@users.noreply.github.com>
2022-02-03 00:12:36 -0800

    Update random.js

--- random.js
+++ random.js
@@ -1,2 +1,6 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+var song = Array("song1", "song2", "song3", "song4", "song5", "song6");
+
+function randomSong() {
+  var randomSong = song[Math.floor(Math.random() * song.length)];
+  document.getElementById('randomSong').value = randomSong;
+}
